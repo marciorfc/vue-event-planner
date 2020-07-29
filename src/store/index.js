3410b6e5ac6596fff1868e50ce7590606a16928f
Marcio Roberto <mroberto@gmail.com>
2020-07-29 10:13:52 -0300

    Create new event in the EventCreate component, adding event to the mock db and to the vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,45 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+import EventService from '@/services/EventService.js'
+
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: { id: 'abc123', name: 'Jane Doe' },
+    events: [],
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
+  getters: {
+    categoryLength: state => state.categories.length,
+    getEventById: state => id => state.todos.find(event => event.id === id)
+  },
   modules: {}
-});
+})
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,123 @@
 <template>
   <div>
-    <h1>Create Event</h1>
+    <h1>Create Event, {{ user.name }}</h1>
+    <p>This event was created by {{ user.id }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+    <p>Total of {{ catLength }} categories</p>
+    <p>Todo selected is {{ getEvent(3) }}</p>
+    <p>Using mapGetters now for id 2 {{ getEventById(2) }}</p>
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+      <h3>Name & describe your event</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
+      </div>
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
+      </div>
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
+      </div>
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select a date" />
+      </div>
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
+<script>
+import Datepicker from 'vuejs-datepicker'
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      event: this.createFreshEvent(),
+      times
+    }
+  },
+  computed: {
+    ...mapState(['user', 'categories']),
+    ...mapGetters(['getEventById']),
+    catLength() {
+      //return this.$store.state.categories.length
+      return this.$store.getters.categoryLength
+    },
+    getEvent() {
+      return this.$store.getters.getEventById
+    }
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch('createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEvent()
+        })
+        .catch(() => {
+          console.log('There was a problem creating your event.')
+        })
+    },
+    createFreshEvent() {
+      const id = Math.floor(Math.random() * 1000)
+      return {
+        id: id,
+        user: this.user,
+        category: '',
+        organizer: this.user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
+  }
+}
+</script>
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
